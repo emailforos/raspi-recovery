2c7f556287b3ce08d1936b5ae7747a80d494b8c9
emailforos <emailforos.es@gmail.com>
2024-03-04 18:23:15 +0100

    Update installb.sh
    Revisado para Raspbian Bookworm

--- installb.sh
+++ installb.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-echo "\n*** Instalando docker ***\n"
+echo "\n*** INSTALANDO DOCKER ***\n"
 # https://docs.docker.com/engine/install/debian/#install-using-the-repository
 sudo apt-get update -y
 sudo apt-get install -y ca-certificates curl
-sudo curl -fsSL get.docker.com | sh
-echo "\n*** Instalando docker-compose ***\n"
-sudo apt-get update sudo apt-get install -y docker-compose
+echo "\n*** Instalando repositorio ***\n"
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+echo "\n*** Instalando paquetes ***\n"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo groupadd docker
 sudo usermod -aG docker ${USER}
 docker-compose --version
